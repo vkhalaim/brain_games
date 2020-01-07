91578450b1ba6975888959619495cdfa2ecd899d
kosheniatko <vasyl.khalaim@ukr.net>
2020-01-07 12:59:37 +0200

    Added makefile and greetings to user function.

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,13 @@
 #! /home/vkhalaim/hexlet/bin/python
 
+from brain_games.cli import run
+
 def start():
     print("Welcome to the Brain Games!")
 
 def main():
     start()
+    run()
 
 if __name__ == '__main__':
     main()
